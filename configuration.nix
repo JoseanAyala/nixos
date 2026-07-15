2c6e6adf6b54be84c853c5eb49d6d7e7490f2147
JoseanAyala <joseanayala@users.noreply.github.com>
2026-07-15 01:08:46 -0400

    refactor: modularize configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,9 +4,10 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./modules/1password.nix
+      ./modules/audio.nix
+      ./modules/kde.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -30,64 +31,17 @@
 
   # Enable the fish shell system-wide
   programs.fish.enable = true;
-  # Force fish to be the default login shell for all users
   users.defaultUserShell = pkgs.fish;
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."josean" = {
     isNormalUser = true;
     description = "Josean";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kdePackages.kate
+      git
     ];
   };
 
-
-  environment.systemPackages = with pkgs; [
-    git
-    just
-  ];
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05"; # Did you read the comment?
 }
--- git.nix
+++ git.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   home.packages = [
+    pkgs.git
     pkgs.gh
     pkgs.lazygit
+    pkgs.just
   ];
 }
