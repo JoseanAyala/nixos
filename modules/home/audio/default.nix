22ab2b83aebc8b507de7be49ddff76afc37b5fbb
JoseanAyala <joseanayala@users.noreply.github.com>
2026-07-27 20:49:11 -0400

    add: podmic settings

--- modules/home/audio/default.nix
+++ modules/home/audio/default.nix
@@ -1,10 +1,6 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   # EasyEffects: a software effects rack for PipeWire, like a RØDECaster/GoXLR.
   services.easyeffects.enable = true;
-
-  home.packages = with pkgs; [
-    rnnoise-plugin
-  ];
 
   xdg.dataFile."easyeffects/input/Podmic.json".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/home/audio/easyeffects/input/Podmic.json";
